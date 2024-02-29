19a35d94d31bf6ab9f2578a1a5d6319e985f9d99
AbirBokhtiar <99033112+AbirBokhtiar@users.noreply.github.com>
2024-03-01 01:13:24 +0600

    Add files via upload

---
+++ Absolute Beginner long contest/C/Math.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int q;
+    cin>>q;
+    for(int i=0;i<q;i++)
+    {
+        int l,r;
+        cin>>l>>r;
+        int sum=0;
+        if(r>n) return 0;
+        for(int j=l;j<=r;j++)
+        {
+            sum+=arr[j];
+        }
+        cout<<sum<<endl;
+    }
+    return 0;
+}

4bdd49b6133d965c0a41d1d6f319b0d2e9803cd1
AbirBokhtiar <abirbokhtiar107@gmail.com>
2024-04-15 10:33:49 +0600

    Add files via upload

---
+++ STL/Maps/UnorderedMapQues.cpp
@@ -0,0 +1,28 @@
+/*
+# Given N strings and Q queries. In each query you are given a string.
+  Print frequency of that string. (N <=10^6, S <=100, Q <=10^6)
+
+=> Here, Frequency = Number of times Occurred.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   unordered_map<string,int> m;
+   int N;
+   cin>>N;
+   for(int i=0; i<N; i++){
+    string s;
+    cin>>s;
+    m[s]++;
+   }
+   int q;
+   cin>>q;
+   while(q--){
+    string s;
+    cin>>s;
+    cout<<m[s]<<endl;
+   }
+}
